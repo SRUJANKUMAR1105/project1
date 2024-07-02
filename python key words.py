732dce17b2e6766105557d8bc2505349da03dba3
SRUJANKUMAR1105 <srujanpeter25@gmail.com>
2024-07-02 22:26:05 +0530

    Add files via upload

---
+++ python key words.py
@@ -0,0 +1,50 @@
+# true flase
+print(5==5)
+print(5>5)
+
+
+#
+#using test > stg
+name="srujan"
+Name="divya"
+print(name)
+print(Name)
+
+a=6
+b=7
+c=8
+d=(a+b)*c
+print(d)
+
+x=6
+y=4
+v=5
+d=(100)
+print(d)
+print(x)
+
+#python lista
+
+
+nums=[5,6,7,8,9,10,]
+print(nums)
+nums[0]
+print(0)
+nums[2]
+nums[-1]
+nums[-1]
+print(nums)
+
+#adding flote value
+nums.append(9.5)
+print(nums)
+nums.append("srujan kkumar barigrla")
+print(nums)
+nums.append("true")
+print(nums)
+
+
+fruites=["apple","bananna","mango"]
+print(fruites)
+fruites.insert["pineapple"]
+print(fruites)
